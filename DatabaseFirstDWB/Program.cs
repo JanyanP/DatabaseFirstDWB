3c293834ae849bf7657e6eff9975c5e040b2bda0
Brayan Montoya <65091796+BrayanMontoya@users.noreply.github.com>
2021-02-26 14:38:11 -0600

    Select Pendiente

--- DatabaseFirstDWB/Program.cs
+++ DatabaseFirstDWB/Program.cs
@@ -4,9 +4,23 @@ namespace DatabaseFirstDWB
 {
     class Program
     {
+        var dbContext = new NorthwindContext();
+
+            var employeeQry = dbContext.Employees.Select(s => new
+            {
+                s.TitleOfCourtesy,
+                s.FirstName,
+                s.LastName
+            }).Where(w => w.TitleOfCourtesy == "Mr.");
+
+            var output = employeeQry.ToList();
+            Console.WriteLine(output);
+            output.ForEach(fe => Console.WriteLine("Nombre: " + fe.FirstName));
+        }
+      
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Exciserse1();
         }
     }
 }
